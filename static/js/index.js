6146386a9c255f62c34c5732820a7fcb4e89c211
Vu Own <bhuwanaryal19@gmail.com>
2019-05-16 11:43:49 +0545

    Reorganize file structure

---
+++ static/js/index.js
@@ -0,0 +1,80 @@
+
+	   var mousePressed = false;
+	   var lastX, lastY;
+	   var ctx;
+	   var csrftoken = $('meta[name=csrf-token]').attr('content')
+	   $.ajaxSetup({
+		   beforeSend: function(xhr, settings) {
+			   if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+				   xhr.setRequestHeader("X-CSRFToken", csrftoken)
+			   }
+		   }
+	   })
+	   function init() {
+		   canvas = document.getElementById('myCanvas')
+		   ctx = canvas.getContext("2d");
+		   ctx.fillStyle = 'black';
+		   ctx.fillRect(0, 0, canvas.width, canvas.height);
+		   $('#myCanvas').mousedown(function (e) {
+			   mousePressed = true;
+			   Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+		   });
+		   $('#myCanvas').mousemove(function (e) {
+			   if (mousePressed) {
+				   Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+			   }
+		   });
+		   $('#myCanvas').mouseup(function (e) {
+			   mousePressed = false;
+		   });
+			   $('#myCanvas').mouseleave(function (e) {
+			   mousePressed = false;
+		   });
+	   }
+	   function Draw(x, y, isDown) {
+		   if (isDown) {
+			   ctx.beginPath();
+			   ctx.strokeStyle = $('#selColor').val();
+			   ctx.lineWidth = $('#selWidth').val();
+			   ctx.lineJoin = "round";
+			   ctx.moveTo(lastX, lastY);
+			   ctx.lineTo(x, y);
+			   ctx.closePath();
+			   ctx.stroke();
+		   }
+		   lastX = x; lastY = y;
+	   }
+		   
+	   function clearArea() {
+		   // Use the identity matrix while clearing the canvas
+		   ctx.setTransform(1, 0, 0, 1, 0, 0);
+		   ctx.fillStyle = 'black';
+		   ctx.fillRect(0, 0, canvas.width, canvas.height);
+		   document.getElementById("result").style.display = "none"
+	   }
+		
+	   function save() {
+		   document.getElementById("canvasimg").style.border = "2px solid";
+		   var dataURL = canvas.toDataURL();
+		   document.getElementById("canvasimg").src = dataURL;
+		   document.getElementById("canvasimg").style.display = "inline";
+	   }
+	   function UploadPic() {
+		   // Generate the image data
+		   var Pic = document.getElementById("myCanvas").toDataURL("image/png");
+		   Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "")
+		   // Sending the image data to Server
+		   $.ajax({
+			   type: 'POST',
+			   url: '/charrecognize',
+			   data: JSON.stringify({ image : Pic }),
+			   contentType: 'application/json;charset=UTF-8',
+			   dataType: 'json',
+			   success: function(msg,status, jqXHR){
+					 var a = JSON.parse(jqXHR.responseText);
+					 var prediction = a.prediction
+					 document.getElementById("pred").innerHTML = prediction
+					 document.getElementById("result").style.display = "inline"
+			   }
+		   });
+	   }
